12bc1bac868c67bcf04d3a91d14e71a00b9ae750
Jeff Escalante <jescalan@users.noreply.github.com>
2016-08-09 16:27:45 -0400

    fresh start

---
+++ README.md
@@ -0,0 +1,47 @@
+# Reshape Custom Elements
+
+[![npm](https://img.shields.io/npm/v/reshape-custom-elements.svg?style=flat-square)](https://npmjs.com/package/reshape-custom-elements)
+[![tests](https://img.shields.io/travis/reshape/custom-elements.svg?style=flat-square)](https://travis-ci.org/reshape/custom-elements?branch=master)
+[![dependencies](https://img.shields.io/david/reshape/custom-elements.svg?style=flat-square)](https://david-dm.org/reshape/custom-elements)
+[![coverage](https://img.shields.io/coveralls/reshape/custom-elements.svg?style=flat-square)](https://coveralls.io/r/reshape/custom-elements?branch=master)
+
+Transform custom element names into class names.
+
+## Installation
+
+```sh
+npm i reshape-custom-elements --save
+```
+
+## Usage
+
+```js
+const reshape = require('reshape')
+const customElements = require('reshape-custom-elements')
+
+const html = `<my-component>
+                <my-tex class="text">Text</my-text>
+              </my-component>`
+
+reshape({ plugins: custom({ defaultTag: 'span' }))
+  .process(component)
+  .then((res) => console.log(result.output()))
+```
+
+```html
+<span class="my-component">
+  <span class="my-text text">Text</span>
+</span>
+```
+
+## Options
+
+| Name | Description | Default |
+| ---- | ----------- | ------- |
+| **defaultTag** | Tag used to replace the custom element tag name | `div` |
+| **skipTags** | Array of tags to be processed despite being a normal html tag | `[]`
+
+## License
+
+- Licensed under [MIT](LICENCE.md)
+- See our [contributing guidelines](contributing.md)
---
+++ test/test.js
@@ -0,0 +1,51 @@
+const reshape = require('reshape')
+const test = require('ava')
+const customElements = require('..')
+
+test('basic', (t) => {
+  const html = '<custom>Test</custom>'
+  const expected = '<div class="custom">Test</div>'
+  return compare(t, html, expected)
+})
+
+test('add to existing class', (t) => {
+  const html = '<custom class="test">Test</custom>'
+  const expected = '<div class="test custom">Test</div>'
+  return compare(t, html, expected)
+})
+
+test('class already exists', (t) => {
+  const html = '<custom class="custom">Test</custom>'
+  const expected = '<div class="custom">Test</div>'
+  return compare(t, html, expected)
+})
+
+test('html tag match', (t) => {
+  const html = '<header class="custom">Test</header>'
+  const expected = '<header class="custom">Test</header>'
+  return compare(t, html, expected)
+})
+
+test('undefined options', (t) => {
+  const html = '<div>Test</div>'
+  const expected = '<div>Test</div>'
+  return compare(t, html, expected, undefined)
+})
+
+test('defaultTag', (t) => {
+  const html = '<custom class="custom">Test</custom>'
+  const expected = '<span class="custom">Test</span>'
+  return compare(t, html, expected, { defaultTag: 'span' })
+})
+
+test('skip tags option', (t) => {
+  const html = '<header class="custom">Test</header>'
+  const expected = '<div class="custom header">Test</div>'
+  return compare(t, html, expected, { skipTags: ['header'] })
+})
+
+function compare (t, html, expected, options = {}) {
+  return reshape({ plugins: [customElements(options)] })
+    .process(html)
+    .then((res) => { t.is(res.output(), expected) })
+}
