46df66e7a8aab0eea46d3333074da805a4e348a8
Ravi van Rooijen <ravivanrooijen@live.nl>
2019-06-03 21:24:12 +0200

    Some additional features (#23)
    * Update deps + rename options for clarity
    
    * Add typescript definitions
    
    reshapeCustomElements(..) returns a plain 'Function' for now since the core reshape module doesn't include definitions yet
    
    * Make it possible to locally override the default replacement tag
    
    This may not seem like a useful feature at first (since we're typing both the class an tag name again), but in my opinion it helps convey *semantic* meaning rather than *syntactical* meaning when editing templates.
    
    Possible improvement points:
    - Not sure how I'm supposed to handle multiple attribute values here
    - pretty-quick is being a bit weird, are you sure this is how you want it to be formatted?
    - Maybe a different default attribute name?
    - Maybe a shorter option name?
    - The replacementTag option (#22) should probably be renamed to defaultReplacementTag
    
    * Custom tag ↔ replacement tag map option
    
    The README could probably be worded better
    
    * Move the HTML in the README to it's own (syntax highlighted) section
    
    * Add a generic blacklist
    
    * Change the replacementTagMap to a more convenient format
    
    * Make sure option names are consistent and update TS definitions
    
    * Infer additionalTags from replacementTagMap

--- README.md
+++ README.md
@@ -15,31 +15,75 @@ npm i reshape-custom-elements --save
 
 ## Usage
 
+### Input HTML
+
+```html
+<my-component>
+  <my-text class="text">Text</my-text>
+
+  <!-- An actual HTML element defined in additionalTags -->
+  <label>Label</label>
+
+  <!-- Overriding the default replacement tag with a map -->
+  <my-footer>
+    Reshape is licensed under the MIT license
+  </my-footer>
+
+  <!-- Locally overriding the default replacement tag with an attribute -->
+  <my-text data-replacement="div">
+    This will get wrapped in a div instead of a span
+  </my-text>
+</my-component>
+```
+
+### Reshape processing
+
 ```js
 const reshape = require('reshape')
 const customElements = require('reshape-custom-elements')
 
-const html = `<my-component>
-                <my-text class="text">Text</my-text>
-              </my-component>`
-
-reshape({plugins: [customElements({defaultTag: 'span'})]})
+reshape({
+  plugins: [
+    customElements({
+      replacementTag: 'span',
+      additionalTags: ['label'],
+      replacementTagMap: {
+        footer: ['my-footer']
+      }
+    })
+  ]
+})
   .process(html)
-  .then((res) => console.log(res.output()))
+  .then(res => console.log(res.output()))
 ```
+
+### Output HTML
 
 ```html
 <span class="my-component">
   <span class="my-text text">Text</span>
+
+  <span class="label">Label</span>
+
+  <div class="my-text">
+    This will get wrapped in a div instead of a span
+  </div>
+
+  <footer class="my-footer">
+    Reshape is licensed under the MIT license
+  </footer>
 </span>
 ```
 
 ## Options
 
-| Name | Description | Default |
-| ---- | ----------- | ------- |
-| **defaultTag** | Tag used to replace the custom element tag name | `div` |
-| **skipTags** | Array of tags to be processed despite being a normal html tag | `[]`
+| Name                                | Description                                                                                                                                                | Default            |
+| ----------------------------------- | ---------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------ |
+| **defaultReplacementTag**           | Tag used to replace the custom element tag name                                                                                                            | `div`              |
+| **additionalTags**                  | Array of tags to be processed despite being a normal HTML tag. HTML tags that are used in the `replacementTagMap` will automatically be added to this list | `[]`               |
+| **blacklist**                       | Array of tags that should never be processed                                                                                                               | `[]`               |
+| **replacementTagMap**               | Object containing custom tag ↔ replacement tag mappings in the format: `ReplacedTag: [ customTag1, customTag2, ... ]`. Overrides `replacementTag`          | `{}`               |
+| **replacementTagOverrideAttribute** | Attribute name that can be used to locally override the used replacement tag. Overrides `replacementTag` and `replacementTagMap`                           | `data-replacement` |
 
 ## License
 
--- test/test.js
+++ test/test.js
@@ -2,50 +2,97 @@ const reshape = require('reshape')
 const test = require('ava')
 const customElements = require('..')
 
-test('basic', (t) => {
+test('basic', t => {
   const html = '<custom>Test</custom>'
   const expected = '<div class="custom">Test</div>'
   return compare(t, html, expected)
 })
 
-test('add to existing class', (t) => {
+test('add to existing class', t => {
   const html = '<custom class="test">Test</custom>'
   const expected = '<div class="test custom">Test</div>'
   return compare(t, html, expected)
 })
 
-test('class already exists', (t) => {
+test('class already exists', t => {
   const html = '<custom class="custom">Test</custom>'
   const expected = '<div class="custom">Test</div>'
   return compare(t, html, expected)
 })
 
-test('html tag match', (t) => {
+test('html tag match', t => {
   const html = '<header class="custom">Test</header>'
-  const expected = '<header class="custom">Test</header>'
+  const expected = html
   return compare(t, html, expected)
 })
 
-test('undefined options', (t) => {
+test('undefined options', t => {
   const html = '<div>Test</div>'
-  const expected = '<div>Test</div>'
+  const expected = html
   return compare(t, html, expected, undefined)
 })
 
-test('defaultTag', (t) => {
+test('defaultReplacementTag', t => {
   const html = '<custom class="custom">Test</custom>'
   const expected = '<span class="custom">Test</span>'
-  return compare(t, html, expected, { defaultTag: 'span' })
+  return compare(t, html, expected, { defaultReplacementTag: 'span' })
 })
 
-test('skip tags option', (t) => {
+test('additionalTags option', t => {
   const html = '<header class="custom">Test</header>'
   const expected = '<div class="custom header">Test</div>'
-  return compare(t, html, expected, { skipTags: ['header'] })
+  return compare(t, html, expected, { additionalTags: ['header'] })
 })
 
-function compare (t, html, expected, options) {
+test('backwards compatibility', t => {
+  const html = '<header class="custom">Test</header>'
+  const expected = '<span class="custom header">Test</span>'
+  return compare(t, html, expected, {
+    defaultTag: 'span',
+    skipTags: ['header']
+  })
+})
+
+test('custom replacement tag', t => {
+  const html = '<custom data-replacement="span">Test</custom>'
+  const expected = '<span class="custom">Test</span>'
+  return compare(t, html, expected)
+})
+
+test('custom replacement tag attribute', t => {
+  const html = '<custom tag="span">Test</custom>'
+  const expected = '<span class="custom">Test</span>'
+  return compare(t, html, expected, { replacementTagOverrideAttribute: 'tag' })
+})
+
+test('custom replacement tag map', t => {
+  const html = '<custom>Test</custom>'
+  const expected = '<span class="custom">Test</span>'
+  return compare(t, html, expected, { replacementTagMap: { span: ['custom'] } })
+})
+
+test('infer additional tags from replacement tag map', t => {
+  const html = '<nav>Test</nav>'
+  const expected = '<div class="nav">Test</div>'
+  return compare(t, html, expected, { replacementTagMap: { div: ['nav'] } })
+})
+
+test('custom replacement tag map overriding', t => {
+  const html = '<custom data-replacement="span">Test</custom>'
+  const expected = '<span class="custom">Test</span>'
+  return compare(t, html, expected, { replacementTagMap: { div: ['custom'] } })
+})
+
+test("don't replace blacklisted tags", t => {
+  const html = '<blacklisted>Keep my tag!</blacklisted>'
+  const expected = html
+  return compare(t, html, expected, { blacklist: ['blacklisted'] })
+})
+
+function compare(t, html, expected, options) {
   return reshape({ plugins: [customElements(options)] })
     .process(html)
-    .then((res) => { t.is(res.output(), expected) })
+    .then(res => {
+      t.is(res.output(), expected)
+    })
 }
